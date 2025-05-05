42c7b75f908caa626d59bfeb99abf19dfdb17125
Haisam <haisam101001@gmail.com>
2025-05-05 23:26:49 +0600

    new login

--- Ats/src/ats/FXMLDocumentController.java
+++ Ats/src/ats/FXMLDocumentController.java
@@ -6,10 +6,15 @@ package ats;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -17,18 +22,27 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField userID;
+    @FXML
+    private TextField userPass;
+    @FXML
+    private ChoiceBox<String> whoSelect;
+    
+    ObservableList<String> list = FXCollections.observableArrayList("Student","Teacher","Admin");
+    @FXML
+    private Button u_ID_pass_Submit;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        whoSelect.setItems(list);
+        whoSelect.setValue("Who are you?");
     }    
+
+    @FXML
+    private void u_ID_pass_Submit(ActionEvent event) {
+    }
     
 }
