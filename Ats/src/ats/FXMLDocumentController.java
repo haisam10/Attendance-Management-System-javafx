5dd7bd49ebad339815107d4199d56b49f48c8554
Haisam <haisam101001@gmail.com>
2025-05-05 21:23:26 +0600

    Ats-new

--- Ats/src/ats/FXMLDocumentController.java
+++ Ats/src/ats/FXMLDocumentController.java
@@ -2,14 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package a.t.m.s.javafx;
+package ats;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -18,13 +17,12 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    private Label label;
     @FXML
-    private Button button;
+    private Label label;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me");
+        System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
     
