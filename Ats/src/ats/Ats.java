c6af8efab79d2cd58013b6326b4e72c4b2a5fa7c
Haisam <haisam101001@gmail.com>
2025-06-02 22:55:48 +0600

    2/6/2025

--- Ats/src/ats/Ats.java
+++ Ats/src/ats/Ats.java
@@ -21,7 +21,7 @@ public class Ats extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+//        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
