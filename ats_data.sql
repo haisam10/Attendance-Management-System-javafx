c59cdc1f6fac8a511f56fa5ee15156e36030e821
Haisam <haisam101001@gmail.com>
2025-07-26 02:20:59 +0600

    Update ats_data.sql

--- ats_data.sql
+++ ats_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2025 at 02:26 PM
+-- Generation Time: Jul 25, 2025 at 10:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,10 +29,36 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `attendance` (
   `id` int(11) NOT NULL,
-  `student_id` varchar(20) DEFAULT NULL,
+  `student_id` varchar(50) DEFAULT NULL,
+  `student_name` varchar(100) DEFAULT NULL,
+  `section` varchar(10) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `student_id`, `student_name`, `section`, `date`, `status`) VALUES
+(1, '223071102', NULL, NULL, '2025-07-25', 1),
+(2, '223071103', NULL, NULL, '2025-07-25', 1),
+(3, '223071104', NULL, NULL, '2025-07-25', 1),
+(4, '223071105', NULL, NULL, '2025-07-25', 1),
+(5, '223071106', NULL, NULL, '2025-07-25', 0),
+(6, '223071114', NULL, NULL, '2025-07-25', 1),
+(7, '223071102', NULL, NULL, '2025-07-26', 1),
+(8, '223071103', NULL, NULL, '2025-07-26', 1),
+(9, '223071104', NULL, NULL, '2025-07-26', 0),
+(10, '223071105', NULL, NULL, '2025-07-26', 1),
+(11, '223071106', NULL, NULL, '2025-07-26', 0),
+(12, '223071114', NULL, NULL, '2025-07-26', 1),
+(13, '223071102', NULL, NULL, '2025-07-24', 1),
+(14, '223071103', NULL, NULL, '2025-07-24', 1),
+(15, '223071104', NULL, NULL, '2025-07-24', 0),
+(16, '223071105', NULL, NULL, '2025-07-24', 1),
+(17, '223071106', NULL, NULL, '2025-07-24', 1),
+(18, '223071114', NULL, NULL, '2025-07-24', 1);
 
 -- --------------------------------------------------------
 
@@ -54,10 +80,10 @@ CREATE TABLE `students` (
 --
 
 CREATE TABLE `student_info` (
-  `id` int(11) NOT NULL,
+  `student_id` varchar(20) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `student_id` varchar(50) DEFAULT NULL,
-  `department` varchar(100) DEFAULT NULL,
+  `department` varchar(50) DEFAULT NULL,
+  `section` varchar(10) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -66,9 +92,15 @@ CREATE TABLE `student_info` (
 -- Dumping data for table `student_info`
 --
 
-INSERT INTO `student_info` (`id`, `name`, `student_id`, `department`, `email`, `phone`) VALUES
-(1, 'asa', '1234', 'cse', 'asdfa@gmail.com', '12346'),
-(2, 'haisam', '1', 'cser', 'asca', '564165');
+INSERT INTO `student_info` (`student_id`, `name`, `department`, `section`, `email`, `phone`) VALUES
+('223071102', 'sourov', 'CSE', 'C', 'sourov@mail.com', '123456'),
+('223071103', 'titi', 'CSE', 'C', 'titi@mail.com', '123456'),
+('223071104', 'shakil', 'CSE', 'C', 'shakil@mail.com', '123456'),
+('223071105', 'meam', 'CSE', 'C', 'meam@mail.com', '123456'),
+('223071106', 'nir', 'CSE', 'C', 'nir@mail.com', '123456'),
+('223071107', 'Arshi', 'CSE', 'B', 'arshi', '123456'),
+('223071108', 'tanzila', 'CSE', 'C', 'tanzila@mail.com', '123456'),
+('223071114', 'MD Haisam Hoque', 'CSE', 'C', 'mdhh.info@gmail.com', '123456');
 
 -- --------------------------------------------------------
 
@@ -146,7 +178,7 @@ ALTER TABLE `students`
 -- Indexes for table `student_info`
 --
 ALTER TABLE `student_info`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`student_id`);
 
 --
 -- Indexes for table `test`
@@ -168,13 +200,7 @@ ALTER TABLE `timetable`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `student_info`
---
-ALTER TABLE `student_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `test`
